e234cb3f44282d867eee638d43b421b6b0654240
Alain Dutech <snowgoon88@gmail.com>
2018-08-14 16:10:54 +0200

    BUG in textures but progressing

--- src/matrix2020/gl_environment.hpp
+++ src/matrix2020/gl_environment.hpp
@@ -12,6 +12,7 @@
 
 #include <gl_shader.hpp>
 #include <SOIL/SOIL.h>               // Load images
+#include <stb_image.h>               // alt to load images
 
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
@@ -132,25 +133,56 @@ public:
     // glBindVertexArray( 0 );
     
     // load and create texture
-    //std::string filename{"data/stripe_yel_80x80.png"};
-    //std::string filename{"/home/alain/Projets/ChuchuGL/data/stripe_yel_80x80.png"};
-    std::string filename{"/home/alain/Projets/ChuchuGL/Images/tex_titlerocket.png"};
-    std::cout << "__SOIL with " << filename  << std::endl;
-    //glActiveTexture(GL_TEXTURE0);
-    std::cout << "  active texture" << std::endl;
-    _texture_id = SOIL_load_OGL_texture
-      (
-       filename.c_str(),                         // pathfile
-       //"Images/tex_titlerocket.png",
-       //"/home/alain/Projets/ChuchuGL/data/stripe_yel_80x80.png",
-       SOIL_LOAD_AUTO,                           // 
-       SOIL_CREATE_NEW_ID,                       //
-       SOIL_FLAG_INVERT_Y                        // Corrige les Y upside/down
-       );
-    std::cout << "  soil finished" << std::endl;
-    if(_texture_id == 0)
-      std::cerr << "GLSprite: SOIL loading error: '" << SOIL_last_result() << "' (" << filename << ")" << std::endl;
+    // //std::string filename{"data/stripe_yel_80x80.png"};
+    // //std::string filename{"/home/alain/Projets/ChuchuGL/data/stripe_yel_80x80.png"};
+    // std::string filename{"/home/alain/Projets/ChuchuGL/Images/tex_titlerocket.png"};
+    // std::cout << "__SOIL with " << filename  << std::endl;
+    // //glActiveTexture(GL_TEXTURE0);
+    // std::cout << "  active texture" << std::endl;
+    // _texture_id = SOIL_load_OGL_texture
+    //   (
+    //    filename.c_str(),                         // pathfile
+    //    //"Images/tex_titlerocket.png",
+    //    //"/home/alain/Projets/ChuchuGL/data/stripe_yel_80x80.png",
+    //    SOIL_LOAD_AUTO,                           // 
+    //    SOIL_CREATE_NEW_ID,                       //
+    //    SOIL_FLAG_INVERT_Y                        // Corrige les Y upside/down
+    //    );
+    // std::cout << "  soil finished" << std::endl;
+    // if(_texture_id == 0)
+    //   std::cerr << "GLSprite: SOIL loading error: '" << SOIL_last_result() << "' (" << filename << ")" << std::endl;
 
+    glGenTexture( 1, &_texture_id);
+    glBindTexture( GL_TEXTURE_2D, _texture_id );
+    // set wrapping parameters (repeat, usual default)
+    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT );
+    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT );
+    // set filtering
+    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTERING, GL_LINEAR );
+    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTERING, GL_LINEAR );
+    // load image, create texture and generate mipmap
+    int img_w, img_h, img_nb_channels;
+    stbi_set_flip_vertically_on_load( true ); // flip image on y-axis
+    unsigned char *img_data = stbi_load( filename.c_str(),
+                                         &img_w, &img_h, &img_nb_channels, 0 );
+    if (img_data) {
+      glTexImage2D( GL_TEXTURE_2D, 0,
+                    GL_RGB, img_w, img_h, 0,
+                    GL_RGB, GL_UNSIGNED_BYTE, img_data );
+      glGenerateMipmap( GL_TEXTURE_2D );
+    }
+    else {
+      std::cerr << "GLSprite: STB loading error:  (" << filename << ")" << std::endl;
+    }
+
+    stbi_image_free(img_data);
+
+    // tell OpenGl for each sampler to which texture it belongs to
+    XXX.use();
+    glUniformli( glGetUniformLocation( ourShader.ID, "texture1"), 0);
+    ou
+      ourShader(.setInt( "texture1", 0 );
+                                         
   }
   // *************************************************** GLEnvironment::render
   void render()
