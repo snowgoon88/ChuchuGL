cae7f9b168c3a3095ee8e34715ad465add88d78e
Alain <snowgoon88@gmail.com>
2018-09-21 01:34:55 +0200

    ADD empty test-007-dyn-vbo with TODO

---
+++ test/matrix2020/test-007-dyn-vbo.cpp
@@ -0,0 +1,19 @@
+/* -*- coding: utf-8 -*- */
+
+/** 
+ * Implement a dymamic VBO with list of vertex that are origin of QUADS
+ * with RGBA colors drawn using a geometry shader.
+ *
+ * In a first time : 2 square moving right :o)
+ */
+
+/* 
+https://www.khronos.org/opengl/wiki/Vertex_Specification_Best_Practices
+https://www.khronos.org/opengl/wiki/Buffer_Object_Streaming
+http://forum.lwjgl.org/index.php?topic=5334.0
+https://www.opengl.org/discussion_boards/showthread.php/178828-VBO-with-dynamically-changing-number-of-points-%21%21/page2
+http://www.songho.ca/opengl/gl_vbo.html#example3
+*/
+
+
+#endif // CLASS_HPP
