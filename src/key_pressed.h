342bdcd74f8e65a6e39fa2d672f014a945b6ddd3
Alain Dutech <snowgoon88@gmail.com>
2019-01-03 21:23:11 +0100

    ADD wait for key pressed

---
+++ src/key_pressed.h
@@ -0,0 +1,87 @@
+/* -*- coding: utf-8 -*- */
+
+#ifndef KEY_PRESSED_HPP
+#define KEY_PRESSED_HPP
+
+/** 
+ * Implement a function to test if keyboard was hit.
+ * Blocking wait :  wait_keypressed()
+ */
+
+#include <iostream>
+//#include <stdio.h>
+#include <termios.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+/**
+ * Non blocking test to see if keyboard was hit
+ * by Vinay Sajip, comments by Lucas
+ * https://stackoverflow.com/questions/1449324/how-to-simulate-press-any-key-to-continue/9419951#9419951
+ *
+ * There is also a nice C++ implementation at 
+ * https://gist.github.com/johnsyweb/1910950
+ *
+ * Example : wait for a key
+ * printf("Press any key:");
+ * fflush(stdout);
+ * while (!kbhit())
+ *     ;
+ * puts("Done");
+ */
+int kbhit(void)
+{
+  // the struct termios stores all kinds of flags which can
+  // manipulate the I/O Interface
+  // => an old one to save the old settings and a new 
+  struct termios oldt, newt;
+  int ch;
+  int oldf;
+
+  // tcgetattr gets the parameters of the current terminal
+  // STDIN_FILENO will tell tcgetattr that it should write the settings
+  // of stdin to oldt
+  tcgetattr(STDIN_FILENO, &oldt);
+  //now the settings will be copied
+  newt = oldt;
+
+  //two of the c_lflag will be turned off
+  //ECHO which is responsible for displaying the input of the user in the terminal
+  //ICANON is the essential one!
+  //Normally this takes care that one line at a time will be processed
+  //that means it will return if it sees a "\n" or an EOF or an EOL
+  newt.c_lflag &= ~(ICANON | ECHO);
+
+  //Those new settings will be set to STDIN
+  //TCSANOW tells tcsetattr to change attributes immediately.
+  tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+
+  // The non-blocking part I guess
+  oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
+  fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
+
+  // test keyboard/consol
+  ch = getchar();
+
+  //the old settings will be written back to STDIN
+  tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+  fcntl(STDIN_FILENO, F_SETFL, oldf);
+
+  if(ch != EOF)
+  {
+    ungetc(ch, stdin);
+    return 1;
+  }
+
+  return 0;
+}
+
+void wait_keypressed()
+{
+  std::cout << "Press any key to continue..." << std::endl;
+  while (!kbhit()) {
+    sleep(0);
+  }
+}
+
+#endif // KEY_PRESSED_HPP
---
+++ test/test-008-wait_key.cpp
@@ -0,0 +1,14 @@
+/* -*- coding: utf-8 -*- */
+
+/** 
+ * Test wait_keypressed.
+ */
+
+#include <key_pressed.h>
+
+int main(int argc, char *argv[])
+{
+  wait_keypressed();
+  
+  return 0;
+}
