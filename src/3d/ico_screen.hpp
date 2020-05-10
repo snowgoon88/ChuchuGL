89420aed1d677dd962048b84711771321c7f345a
Alain Dutech <snowgoon88@gmail.com>
2020-05-10 13:14:33 +0200

    ADD textured Icosahedron in 3d/ico_app

--- src/3d/ico_screen.hpp
+++ src/3d/ico_screen.hpp
@@ -26,28 +26,34 @@
 #include <iostream>                       // std::cout
 #include <3d/gl_3dcanvas.hpp>
 #include <3d/gl_3dcanvas_screen.hpp>
-
+#include <SOIL/SOIL.h>                    // Load images
 // ***************************************************************************
 // ***************************************************************** IcoScreen
 // ***************************************************************************
 class IcoScreen : public GL3DCanvasScreen
 {
+  enum class ModeT { Unicolor, VtxColor, Texture };
 public:
   // ************************************************** IcoScreen::constructor
   IcoScreen( GL3DCanvas& canvas ) : GL3DCanvasScreen( canvas ),
     _model( 1.0f ),
-    _mode_param( 0 ), _fade_param( 1.f ),
+    _mode_param( ModeT::Unicolor ), _fade_param( 1.f ),
     _model_rotate_param( false ), _outline_param( false )
   {
+    make_texture_shader( true );
+    build_face_texture( true );
     build_faces_and_lines( true );
     build_axes();
     build_help_msg();
-    _canvas.set_param( _mode_param, _fade_param, true /*forced*/ );
+    set_canvas_param( true /* forced */ );
   }
   // *************************************************** IcoScreen::destructor
   virtual ~IcoScreen()
   {
     _canvas.delete_vao( _ico_faces_vao, _ico_faces_vbo, _ico_idxsides_vbo );
+
+    delete_texfaces();
+    delete_texture_shader( true );
     delete _axes_shader;
   }
   // ***************************************************** IcoScreen::render
@@ -84,27 +90,44 @@ public:
     auto proj_view = vp_track * z_inverter;
 
     // Tetrahedron
+    // First, draw texture
+    if (_mode_param == ModeT::Texture ) {
+      _texture_shader->use();
+      glBindVertexArray( _texface_vao );
+      glUniformMatrix4fv( _u_pv_tex, 1, GL_FALSE, glm::value_ptr(proj_view) );
+      glUniformMatrix4fv( _u_m_tex, 1, GL_FALSE, glm::value_ptr(_model) );
+
+      glActiveTexture( GL_TEXTURE1 );
+      glUniform1i( _u_id_tex, 1 );
+      glBindTexture(GL_TEXTURE_2D, _planet_tex_id );
+    
+      glDrawArrays( GL_TRIANGLES, 0, 20*3 /*nb vtx*/); // mode, first, count
+    }
+
     // TODO: Some call could be made/set outside of render (proj)
     // _smart_shader->use();
     _canvas.smart_shader()->use();
     _canvas.set_projection_view_transform( proj_view );
     _canvas.set_model_transform( _model );
-
+    
     // uniform color
     //glUniform3f( _u_color, 0.f, 0.5f, 0.5f );
     _canvas.set_color_rgb( {0.f, 0.5f, 0.5f}, false /*no force_use*/);
-
-    // draw faces
+    
     glBindVertexArray( _ico_faces_vao );
-    glDrawArrays( GL_TRIANGLES, 0, 20*3 /*nb vtx*/); // mode, first, count
+  
+    // draw faces
+    if (_mode_param != ModeT::Texture ) {
+      glDrawArrays( GL_TRIANGLES, 0, 20*3 /*nb vtx*/); // mode, first, count
+    }
 
     if (_outline_param) {
       // draw sides in white, little outside
-      _canvas.set_param( 1, 1.f, false );
-      _canvas.set_color_rgb( {1.f, 1.f, 1.f} );
+      _canvas.set_param( 1, 1.f, false ); /* need UniformColor */
+      _canvas.set_color_rgb( {1.f, 0.f, 1.f} );
       glDrawElements( GL_LINES, 30*2, GL_UNSIGNED_INT, 0);
       // back to param
-      _canvas.set_param( _mode_param, _fade_param );
+      set_canvas_param( false /* not forced */ );
     }
   
     // and now axes
@@ -137,6 +160,15 @@ public:
     glPopAttrib();
     glUseProgram(0);
   }
+  void set_canvas_param( bool force=false )
+  {
+    if (_mode_param == ModeT::Unicolor ) {
+      _canvas.set_param( 1, _fade_param,  force);
+    }
+    else {
+      _canvas.set_param( 0, _fade_param,  force);
+    }
+  }
   // ************************************************** IcoScreen::attributs
 protected:
   // VAOs and VBOs
@@ -144,10 +176,18 @@ protected:
 
   GLuint _axe_vbo, _axe_vao;
   GLShader* _axes_shader;
+
+  GLShader* _texture_shader;
+  GLuint _u_m_tex, _u_pv_tex, _u_id_tex; 
+  GLuint _a_pos_tex, _a_st_tex;   
+  
+  GLuint _texface_vao, _texface_vtx_vbo;
+  GLuint _planet_tex_id;
+    
   
   // Rendering parameters
   glm::mat4 _model;
-  GLint     _mode_param;
+  ModeT     _mode_param;
   GLfloat   _fade_param;
   bool      _model_rotate_param;
   bool      _outline_param;
@@ -359,6 +399,148 @@ protected:
     glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
   }
+  // ************************************************ IcoSphere::build_texture
+  void build_face_texture( bool verb=false )
+  {
+    // need vertices
+    VTXvec ico_vtx = build_vertex( verb );
+
+    // Hold x,y,z + s,t for every vertex of every face
+    VTXvec texfaces_vtx;
+
+    // Make the tour of the top row and add 4 faces per vtx
+    /**
+    NOTE: The texture coords are offset in order to align coords to image pixels
+    Image width = 11S, height = 3T : S=1/11, T=1/3
+    But in 2048x1024 image with triangles touching "up" we have
+    2S = 372 / 2048 and T= 322 / 1024 
+    //    (S,0)  3S  5S  7S  (9S,0)
+    //       /\  /\  /\  /\  /\             //
+    //      /__\/__\/__\/__\/__\(10S,T)     //
+    // (0,T)\  /\  /\  /\  /\  /\           //
+    //       \/__\/__\/__\/__\/__\(11S,2T)  //
+    //  (S,2T)\  /\  /\  /\  /\  /          //
+    //         \/  \/  \/  \/  \/           //
+    //    (2S,3T)  4S  6S  8S  (10S,3T)
+    */
+    const float T = 322.f / 1024.f;
+    const float S = 186.f / 2048.f;
+    unsigned int i_top = 0;
+    unsigned int i_bottom = 11;
+    
+    for( unsigned int i = 1; i <= 5; ++i) {
+      unsigned int i_next = i+1;
+      unsigned int i_below=i+5;
+      unsigned int i_below_next=i_below+1;
+      if (i==5) {
+        i_next = 1;
+        i_below_next = 6; 
+      }
+      
+      // Top face
+      add_texface( texfaces_vtx, ico_vtx, {i, i_next, i_top},
+                   {{(i-1)*2*S,T},{i*2*S,T},{((i-1)*2+1)*S,0}}, verb);
+      // Side Face 2
+      add_texface( texfaces_vtx, ico_vtx, {i, i_below, i_next},
+                   {{(i-1)*2*S,T},{((i-1)*2+1)*S,2*T},{i*2*S,T}}, verb );
+      // Side Face 3
+      add_texface( texfaces_vtx, ico_vtx, {i_next, i_below, i_below_next},
+                   {{i*2*S,T},{((i-1)*2+1)*S,2*T},{((i-1)*2+3)*S,2*T}}, verb );
+      // Bottom face
+      add_texface( texfaces_vtx, ico_vtx, {i_below, i_bottom, i_below_next},
+                   {{((i-1)*2+1)*S,2*T},{i*2*S,3*T},{((i-1)*2+3)*S,2*T}}, verb );      
+    }
+
+    // VAO, VBO, Texture
+    // VBO for vertex
+    glGenBuffers(1, &_texface_vtx_vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, _texface_vtx_vbo);
+    // push xyz+st into VBO
+    glBufferData(GL_ARRAY_BUFFER, sizeof( GLfloat ) * texfaces_vtx.size(),
+  		 texfaces_vtx.data(), GL_STATIC_DRAW);
+    if (verb) {
+      std::cout << "  pushed " <<  texfaces_vtx.size() << " vtx into texface_vtx_vbo" << std::endl;
+    }
+
+    // VAO with vtx and texture coordinates
+    glGenVertexArrays( 1, &_texface_vao );
+    glBindVertexArray( _texface_vao );
+    glBindBuffer(GL_ARRAY_BUFFER, _texface_vtx_vbo);
+    // location of vtx
+    glVertexAttribPointer( _a_pos_tex, 3, GL_FLOAT, GL_FALSE,
+                           (3+2) * sizeof(float), // stride
+                           (void*)(0 * sizeof(float)) ); // offset
+    glEnableVertexAttribArray( _a_pos_tex);
+    // location of st
+    glVertexAttribPointer( _a_st_tex, 2, GL_FLOAT, GL_FALSE,
+                           (3+2) * sizeof(float), // stride
+                           (void*)(3 * sizeof(float)) ); // offset
+    glEnableVertexAttribArray( _a_st_tex );
+    
+    glBindVertexArray( 0 );
+    
+    if (verb) {
+      std::cout << "__MAKE_VAO _texface_vao=" << _texface_vao;
+      std::cout << " vtx_vbo=" << _texface_vtx_vbo;
+    }
+
+    // Load Texture
+    std::string filename{"data/icosahedron_template1.png"};
+    std::cout << "__SOIL with " << filename  << std::endl;
+
+    std::cout << "  active texture" << std::endl;
+    _planet_tex_id = SOIL_load_OGL_texture
+      (
+       filename.c_str(),                         // pathfile
+       SOIL_LOAD_AUTO,                           // 
+       SOIL_CREATE_NEW_ID,                       //
+       0 // NO INVERT SOIL_FLAG_INVERT_Y         // CorrectY upside/down
+       );
+    std::cout << "  soil finished" << std::endl;
+    if (_planet_tex_id == 0) {
+      std::cerr << "GLSprite: SOIL loading error: '" << SOIL_last_result() << "' (" << filename << ")" << std::endl;
+    }
+    else {
+      std::cout << "__TEXTURE " << filename << " loaded as " <<  _planet_tex_id<< std::endl;
+    }
+  }
+  void delete_texfaces()
+  {
+    glDeleteVertexArrays( 1, &_texface_vao );
+    glDeleteBuffers( 1, &_texface_vtx_vbo );
+
+     glDeleteTextures( 1, &_planet_tex_id );
+  }
+  void add_texface( VTXvec& dest, VTXvec& src,
+                    const std::vector<unsigned int>& idx,
+                    const std::vector<VTXvec>& coord,
+                    bool verb=false )
+  {
+    if (verb) {
+      std::cout << "add_texface ";
+      std::cout << " : (" << idx[0] << ", " << idx[1] << ", " << idx[2];
+      std::cout << ")";
+      std::cout << "  coord = { ";
+      std::cout << "( " << coord[0][0] << ", " << coord[0][1] << "), ";
+      std::cout << "( " << coord[1][0] << ", " << coord[1][1] << "), ";
+      std::cout << "( " << coord[2][0] << ", " << coord[2][1] << ") ";
+      std::cout << "}" << std::endl;
+    }
+     // Vertices
+    unsigned int idx_coord = 0;
+    for( auto& i: idx) {
+      
+      dest.push_back( src[i*3+0] );
+      dest.push_back( src[i*3+1] );
+      dest.push_back( src[i*3+2] );
+
+      // TexCoord
+      dest.push_back( (float) coord[idx_coord][0] );
+      dest.push_back( (float) coord[idx_coord][1] );
+      idx_coord++;
+    }
+  }
+  // ********************************************************* IcoScreen::help
   void build_help_msg()
   {
     _help_msg.push_back( "M1:rot, shift+M1: trans, SCROLL: zoom.   r/R: reset camera");
@@ -368,14 +550,47 @@ protected:
   void update_help_msg()
   {
      std::stringstream help_msg;
-     help_msg << "m/M: mode (" << _mode_param << ")";
-     help_msg << ", f/F: fade (" << _fade_param << ")";
+     help_msg << "m/M: mode (";
+     switch( _mode_param ) {
+     case ModeT::Unicolor:
+       help_msg << "UNI";
+       break;
+     case ModeT::VtxColor:
+       help_msg << "VTX";
+       break;
+     case ModeT::Texture:
+       help_msg << "TEX";
+       break;
+     default:
+       help_msg << "???";
+     }
+     help_msg << "), f/F: fade (" << _fade_param << ")";
      help_msg << ", s/S: stopRot (" << (not _model_rotate_param) << ")";
      help_msg << ", o/O: outline (" << _outline_param << ")";
   
      _help_msg[1] = help_msg.str(); 
   }
-
+  // ****************************************************** IcoScreen::shaders
+  void make_texture_shader( bool verb=false )
+  {
+    if (verb) {
+      std::cout << "__SHADER create _texture_shader" << std::endl;
+    }
+     _texture_shader = new GLShader( "src/shaders/m_vp_pos_st.v330.glsl",
+                                    "src/shaders/texture.f330.glsl" );
+    _u_m_tex = _texture_shader->getUniformLocation( "u_model4x4" );
+    _u_pv_tex = _texture_shader->getUniformLocation( "u_proj_view4x4" );
+    _u_id_tex = _texture_shader->getUniformLocation( "texture_id" );
+    _a_pos_tex = _texture_shader->getAttribLocation( "a_pos3" );
+    _a_st_tex = _texture_shader->getAttribLocation( "a_tex_pos2" );
+  }
+  void delete_texture_shader( bool verb=false )
+  {
+    if (verb) {
+      std::cout << "__SHADER delete _texture_shader" << std::endl;
+    }
+    delete _texture_shader;
+  }
 public:
   // *************************************************** IcoScreen::callback
   virtual void key_callback(int key, int scancode, int action, int mods)
@@ -385,9 +600,21 @@ public:
     
     // M
     if (key == GLFW_KEY_SEMICOLON && action == GLFW_PRESS) {
-      _mode_param = 1 - _mode_param;
+      switch( _mode_param ) {
+      case ModeT::Unicolor:
+        _mode_param = ModeT::VtxColor;
+        break;
+      case ModeT::VtxColor:
+        _mode_param = ModeT::Texture;
+        break;
+      case ModeT::Texture:
+        _mode_param = ModeT::Unicolor;
+        break;
+      default:
+         _mode_param = ModeT::VtxColor;
+      }
       //std::cout << "__PARAM mode=" << _mode_param << std::endl;
-      _canvas.set_param( _mode_param, _fade_param, true /*force*/ );
+      set_canvas_param( true /*force*/ );
       update_help_msg();
     }
     // F
@@ -395,7 +622,7 @@ public:
       _fade_param -= 1.f/3.f;
       if (_fade_param < -0.1f) _fade_param = 1.f;
       //std::cout << "__PARAM fade=" << _fade_param << std::endl;
-      _canvas.set_param( _mode_param, _fade_param, true /*force*/ );
+      set_canvas_param( true /*force*/ );
       update_help_msg();
     }
     // S
